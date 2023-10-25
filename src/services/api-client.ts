a37c5f44cdfd32bde2d1fe54e2d0e6902fb8a898
Petro Arva <petro.arva@itengine.rs>
2023-10-25 13:50:26 +0200

    create custom useGame hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/x api",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "00b4c09cc3ac4f8faec2bfcaef065a58",
   },
