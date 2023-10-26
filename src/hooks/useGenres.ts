2837e378cec3f72937d856216501c1bcb6d2cf01
Petro Arva <petro.arva@itengine.rs>
2023-10-26 16:14:15 +0200

    Refactoring, implement react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,17 @@
-import useData from "./useData";
-
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { FetchResponse } from "../services/api-client";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+  });
 
 export default useGenres;
